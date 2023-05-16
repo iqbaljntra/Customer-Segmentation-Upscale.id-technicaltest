9ee8e445d7a927df8878702180733a1d3fe73893
Iqbaljntra <41693303+iqbaljntra@users.noreply.github.com>
2023-05-17 02:32:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-import streamlit as st
-import joblib
-import pandas as pd
 
-# Load the saved pipeline object
-all_process = joblib.load("all_process.joblib")
-
-Gender = st.radio(('Male','Female'))
-Profession = st.radio(('Healthcare', 'Engineer', 'Lawyer', 'Entertainment', 'Artist',
-       'Executive', 'Doctor', 'Homemaker', 'Marketing'))
-Age = st.slider('Points In Wallet',0, 99)
-Annual Income ($) = st.slider()
